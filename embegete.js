656a9a9d0876ba519471c202e0f65fc24a949e58
Brian Wanyama <89256205+Wanayama@users.noreply.github.com>
2025-02-09 14:15:45 +0300

    update

---
+++ embegete.js
@@ -0,0 +1,10 @@
+ScrollReveal({
+    reset: true,
+    distance: '80px',
+    duration: 2000,
+    delay: 200
+});
+ScrollReveal().reveal('.form-box', {origin: 'top'});
+ScrollReveal().reveal('.htext', {origin: 'left'});
+ScrollReveal().reveal('.htext p', {origin: 'bottom'});
+
